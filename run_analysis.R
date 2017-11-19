5eb4cf1f0df1ac3a9a5b430346dd7d75db07fcdb
vivekpruthi <vivekpruthi@gmail.com>
2017-11-19 13:35:20 -0500

    data unzipped code added

--- run_analysis.R
+++ run_analysis.R
@@ -1,2 +1,3 @@
 if (!file.exists("zipped.zip")){
 download.file(url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",destfile = "zipped.zip")}
+unzip("zipped.zip")
